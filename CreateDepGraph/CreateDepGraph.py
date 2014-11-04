d9251683d93eceadbeeaae766673dfa1f669e02c
Giovanni Yoko Kristianto <giovanni@nii.ac.jp>
2014-11-04 22:24:07 +0900

    refactor

--- CreateDepGraph/CreateDepGraph.py
+++ CreateDepGraph/CreateDepGraph.py
@@ -2,36 +2,61 @@ from xml.dom import minidom
 from os import listdir, path
 
 #Properties
-xhtmlDir = ''
+xhtmlDir = 'D:/Virtual/vartab/'
 tagsWithBase = ['mroot', 'msub', 'msup', 'msubsup', 'munder', 'mover', 'munderover', 'mmultiscripts', ] #consider mstyle
+mathtag = 'm:math'
 
 def __getXHTMLFiles(sourceDir):
-    return [path.join(sourceDir, flName) for flName in listdir(sourceDir)]
-
-def __expandMath(mt):
-    '''
-        mt : xml element
-        return value : list of xml elements
-    '''
-    retval = [mt]
-    for tag in tagsWithBase:
-        children = mt.getElementsByTagName(tag)
-        if len(children) > 0:
-            for child in children:
-                retval.append(child.firstChild)
-    return retval
+    return [path.join(sourceDir, flName) for flName in listdir(sourceDir) if flName.endswith('xhtml')]
 
 def __expandMaths(mts):
     '''
         mts : {idx:<math>}
     '''
+    def __expandMath(mt):
+        '''
+            mt : xml element
+            return value : list of xml elements
+            algorithms: do not use only the base for each tag, but instead combine it with the rest
+        '''
+        retval = [mt]
+        for tag in tagsWithBase:
+            children = mt.getElementsByTagName(tag)
+            if len(children) > 0:
+                for child in children:
+                    retval.append(child.firstChild)
+        return retval
+
     for idx, mt in mts.iteritems():
+        __removeAttributes(mt)
         mts[idx] = __expandMath(mt)
 
+def __removeAttributes(mt):
+    '''
+        mt: an xml element
+    '''
+    if mt.attributes:
+        for key in mt.attributes.keys():
+            mt.removeAttribute(key)
+    for node in mt.childNodes:
+        __removeAttributes(node)
+    return mt
+
+
+def __createDepGraph(mts):
+    '''
+        mts: {idx:[matchers]}
+    '''
+    edges = []
+    for idx, matchers in mts.iteritems():
+        for idx2, matchers2 in mts.iteritems():
+
+
 if __name__ == '__main__':
-    files = __getXHTMLFiles()
+    files = __getXHTMLFiles(xhtmlDir)
     for fl in files:
         xdoc = minidom.parse(fl)
         
         #enumerate if there is no id in the <math> tag
-        mts = {idx:mt for idx,mt in enumerate(xdoc.getElementsByTagName('math'))}
+        mts = {idx:mt for idx,mt in enumerate(xdoc.getElementsByTagName(mathtag))}
+        __expandMaths(mts)
